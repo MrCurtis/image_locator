8afc9acbddc9258e8cc21dfa177456a489a5532b
a <amc@a.(none)>
2014-02-08 23:38:51 +0000

    changed template a bit

--- image_locator_app/views.py
+++ image_locator_app/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 
+from image_locator_app.models import DroneImage
+
 def home(request):
 
     t = loader.get_template('home.html')
-    c = Context({})
+    c = Context({'drone_images': DroneImage.objects.all() })
     
     return HttpResponse(t.render(c))
