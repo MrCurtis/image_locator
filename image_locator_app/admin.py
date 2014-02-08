bd869727866db115e7f4e72267bd313ccdfd8f25
a <amc@a.(none)>
2014-02-08 22:54:12 +0000

    changed models

---
+++ image_locator_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from image_locator_app.models import DroneImage
+
+admin.site.register(DroneImage)
--- image_locator_app/models.py
+++ image_locator_app/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
-class SelectedImage(models.Model):
 
-    image = models.FileField(upload_to = 'drone_images', blank = True)
+class DroneImage(models.Model):
+
+    image = models.FileField(upload_to='drone_images', blank=True)
     lattitude = models.FloatField()
     longtitude = models.FloatField()
-    comment = models.TextField()
+    altitude = models.FloatField()
+    heading = models.FloatField()
+    tilt = models.FloatField()
+    roll = models.FloatField()
+    STATUS_TYPES = ( ('I', 'Important'), ('N', 'Nothing'), )
+    status = models.CharField(max_length=1, choices=STATUS_TYPES, blank=True)
+    interest_type = models.TextField(blank=True) 
+    comment = models.TextField(blank=True)
     
-class RawImages(models.Model):
-
-    filename = models.TextField()
-    image = models.FileField(upload_to = 'drone_images', blank = True)
-    lattitude = models.FloatField()
-    longtitude = models.FloatField()
+  
 
     
     
