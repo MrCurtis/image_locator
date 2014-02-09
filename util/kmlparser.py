615346588436afc251eccde5cf5cee1b4c22941c
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-09 13:27:52 +0000

    Properly handle KMZ in KML parser, return datetime for timestamps

--- util/kmlparser.py
+++ util/kmlparser.py
@@ -1,24 +1,29 @@
+from datetime import datetime
 from zipfile import ZipFile
 
 from lxml import etree
 
 
-def extract_dates(filenames):
+def extract_marks(filenames):
     ns = '{http://www.opengis.net/kml/2.2}'
     m = ns + 'Model/'
     stripns = lambda tag: tag[len(ns):]
     to_dict = lambda tree: dict((stripns(e.tag), e.text) for e in tree)
+    ts = lambda s: datetime.strptime(s[:-6], "%Y-%m-%dT%H:%M:%S")
     data = []
     for filename in filenames:
-        if filename.endswith('kmz'):
-            filename = ZipFile(filename).namelist()[0]
-        with open(filename) as f:
+        def zopen(filename):
+            if filename.endswith('kmz'):
+                z = ZipFile(filename)
+                return z.open(z.namelist()[0])
+            return open(filename)
+        with zopen(filename) as f:
             f.readline()
             doc = etree.parse(f)
             for p in doc.iter(ns + 'Placemark'):
                 try:
                     data.append({
-                        'timestamp': p.find(ns + 'TimeStamp')[0].text,
+                        'timestamp': ts(p.find(ns + 'TimeStamp')[0].text),
                         'location': to_dict(p.find(m + ns + 'Location')),
                         'orientation': to_dict(p.find(m + ns + 'Orientation'))
                     })
@@ -28,4 +33,4 @@ def extract_dates(filenames):
     return data
 
 if __name__ == '__main__':
-    print extract_dates(['../static/images/2013-07-25 19-54-04.tlog.kml'])
+    print extract_marks(['../static/images/2013-07-25 19-54-04.tlog.kml'])
