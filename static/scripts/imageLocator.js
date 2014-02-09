d1a4c146391dea4b652a034b807412f06f48a5cc
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-09 17:01:47 +0000

    Enable/disable circles when enabling form fields

--- static/scripts/imageLocator.js
+++ static/scripts/imageLocator.js
@@ -1,68 +1,75 @@
-
+var waterCircle, foodCircle, shelterCircle, medicalCircle, protectionCircle;
 var setupKeyboard = function(){
-	
 	console.log("--- keyboard setup ---");
+  document.onkeydown = function(e){
 
-	document.onkeydown = function(e){
-		
-			//leftKeyPress
-			if(e.keyCode == '39'){
-				console.log('NEXT');
-				document.getElementById("triageForm").submit();
-			};
-			
-			//--- STATUS ---
-			
-			//Water
-			if(e.keyCode =='49'){
-			   if(document.getElementById("id_water").checked == false) {
-					document.getElementById("id_water").checked = true;
-				}
-				else{ document.getElementById("id_water").checked = false;
-				}
-			};
-			
-			//food
-			if((e.keyCode =='50')){
-				if(document.getElementById("id_food").checked == false) {
-					document.getElementById("id_food").checked = true;
-				}
-				else{ document.getElementById("id_food").checked = false;
-				}
-			};
-			
-			//shelter
-			if(e.keyCode =='51'){
-			   if(document.getElementById("id_shelter").checked == false) {
-					document.getElementById("id_shelter").checked = true;
-				}
-				else{ document.getElementById("id_shelter").checked = false;
-				}
-			};
-			
-			//medical
-			if(e.keyCode =='52'){
-			   if(document.getElementById("id_medicine").checked == false) {
-					document.getElementById("id_medicine").checked = true;
-				}
-				else{ document.getElementById("id_medicine").checked = false;
-				}
-			};
-			
-			//protection
-			if(e.keyCode =='53'){
-			   if(document.getElementById("id_protection").checked == false) {
-					document.getElementById("id_protection").checked = true;
-				}
-				else{ document.getElementById("id_protection").checked = false;
-				}
-			};
-			
-		};
+    //leftKeyPress
+    if(e.keyCode == '39'){
+      console.log('NEXT');
+      document.getElementById("triageForm").submit();
+    };
+
+    //--- STATUS ---
+
+    //Water
+    if(e.keyCode =='49'){
+      if(document.getElementById("id_water").checked == false) {
+        document.getElementById("id_water").checked = true;
+        waterCircle = addMarker('blue', '#33f', 100);
+      } else {
+        document.getElementById("id_water").checked = false;
+        map.removeLayer(waterCircle);
+      }
+    };
+
+    //food
+    if((e.keyCode =='50')){
+      if(document.getElementById("id_food").checked == false) {
+        document.getElementById("id_food").checked = true;
+        foodCircle = addMarker('green', '#3f3', 200);
+      } else {
+        document.getElementById("id_food").checked = false;
+        map.removeLayer(foodCircle);
+      }
+    };
+
+    //shelter
+    if(e.keyCode =='51'){
+      if(document.getElementById("id_shelter").checked == false) {
+        document.getElementById("id_shelter").checked = true;
+        shelterCircle = addMarker('black', '#333', 300);
+      } else {
+        document.getElementById("id_shelter").checked = false;
+        map.removeLayer(shelterCircle);
+      }
+    };
+
+    //medical
+    if(e.keyCode =='52'){
+      if(document.getElementById("id_medicine").checked == false) {
+        document.getElementById("id_medicine").checked = true;
+        medicalCircle = addMarker('red', '#f33', 400);
+      } else {
+        document.getElementById("id_medicine").checked = false;
+        map.removeLayer(medicalCircle);
+      }
+    };
+
+    //protection
+    if(e.keyCode =='53'){
+      if(document.getElementById("id_protection").checked == false) {
+        document.getElementById("id_protection").checked = true;
+        protectionCircle = addMarker('yellow', '#3ff', 500);
+      } else {
+        document.getElementById("id_protection").checked = false;
+        map.removeLayer(protectionCircle);
+      }
+    };
+  };
 }
 
 // on initialisation
 window.onload = function() {
 	console.log("--- Initialising ---");
   setupKeyboard();
-};
+}
--- static/scripts/map_tutorial.js
+++ static/scripts/map_tutorial.js
@@ -1,35 +1,20 @@
-function onMapClick(e)
-{
-    var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-    alert([e.latlng.lat, e.latlng.lng]);
-}
+var map;
 
-function addMarker(latitude, longitude, water, food)
+function addMarker(color, fillColor, radius)
 {   
-    console.log(water);
-    if (water == 'True')
-    {
-        var circle = L.circle([latitude, longitude], 500, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5
-        }).addTo(map);
-    }
-    else if (food == 'True')
-    {
-        var circle = L.circle([latitude, longitude], 500, {
-        color: 'blue',
-        fillColor: 'blue',
-        fillOpacity: 0.5
-        }).addTo(map);
-    }
+  return L.circle(map.getCenter(), radius, {
+    color: color,
+    //fillColor: fillColor || color,
+    //fillOpacity: 0.5
+  }).addTo(map);
 }
 
-var map = L.map('map').setView([51.505, -0.09], 2);
+function initMap(lat, lng) {
+  map = L.map('map').setView([lat, lng], 14);
 
-L.tileLayer("http://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-}).addTo(map);
-
-map.on('click', onMapClick);
+  L.tileLayer("http://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+      maxZoom: 18
+  }).addTo(map);
+  L.marker([lat, lng]).addTo(map);
+}
