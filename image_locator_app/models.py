33aa1ce9bc2f10f4e4642359110eb8f923ff1e91
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-09 13:44:41 +0000

    Models whitespace clean-up

--- image_locator_app/models.py
+++ image_locator_app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class DroneImage(models.Model):
-
     image = models.FileField(upload_to='drone_images', blank=True)
     lattitude = models.FloatField()
     longtitude = models.FloatField()
@@ -10,17 +9,11 @@ class DroneImage(models.Model):
     heading = models.FloatField()
     tilt = models.FloatField()
     roll = models.FloatField()
-    
     water = models.BooleanField(default=False)
     sanitation = models.BooleanField(default=False)
     food = models.BooleanField(default=False)
     shelter = models.BooleanField(default=False)
     medicine = models.BooleanField(default=False)
     protection = models.BooleanField(default=False)
-    obstruction = models.BooleanField(default=False) 
+    obstruction = models.BooleanField(default=False)
     comment = models.TextField(blank=True)
-    
-  
-
-    
-    
