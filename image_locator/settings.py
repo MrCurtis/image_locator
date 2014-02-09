fbd4180be70f465150f3e927d65976cc58b607bc
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-09 18:45:41 +0000

    Add bootstrap form

--- image_locator/settings.py
+++ image_locator/settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
+    'bootstrapform',
     'image_locator_app',
 )
 
